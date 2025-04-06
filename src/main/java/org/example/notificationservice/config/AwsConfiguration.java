eb8d3221dda01f5c25011cc308b829a8d81a9a67
Yohannes Kassa Yimam <37615135+JoAbyssinia@users.noreply.github.com>
2025-04-05 21:46:34 -0400

    notification impl

--- src/main/java/org/example/notificationservice/config/AwsConfiguration.java
+++ src/main/java/org/example/notificationservice/config/AwsConfiguration.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.EnvironmentVariableCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.ses.SesClient;
 import software.amazon.awssdk.services.sqs.SqsClient;
@@ -18,36 +20,38 @@ import java.net.URI;
 @Configuration
 public class AwsConfiguration {
 
-    @Value("${aws.endpoint}")
-    private static String AWS_ENDPOINT;
-    private static final Region DEFUALT_REGION = Region.US_EAST_1;
+    private static final String LOCALSTACK_ENDPOINT =
+            System.getenv().getOrDefault("AWS_ENDPOINT", "http://localhost:4566");
+    private static final String ACCESS_KEY_ID =
+            System.getenv().getOrDefault("AWS_ACCESS_KEY_ID", "000000000000");
+    private static final String ACCESS_KEY_SECRET =
+            System.getenv().getOrDefault("AWS_SECRET_ACCESS_KEY", "000000000000");
+    private static final Region DEFAULT_REGION = Region.US_EAST_1;
 
     @Bean
     public SqsClient sqsClient() {
-     return SqsClient.builder()
-             .region(DEFUALT_REGION)
-             .credentialsProvider(EnvironmentVariableCredentialsProvider.create())
-             .applyMutation(sqsClientBuilder -> {
-                 sqsClientBuilder.endpointOverride(URI.create(AWS_ENDPOINT));
-             })
-             .build();
+        return SqsClient.builder()
+                .region(DEFAULT_REGION)
+                .credentialsProvider(
+                        StaticCredentialsProvider.create(
+                                AwsBasicCredentials.create(ACCESS_KEY_ID, ACCESS_KEY_SECRET)))
+                .endpointOverride(URI.create(LOCALSTACK_ENDPOINT))
+                .build();
     }
 
     @Bean
     public SesClient sesClient() {
         return SesClient.builder()
-                .region(DEFUALT_REGION)
-                .credentialsProvider(EnvironmentVariableCredentialsProvider.create())
-                .applyMutation(sqsClientBuilder -> {
-                    sqsClientBuilder.endpointOverride(URI.create(AWS_ENDPOINT));
-                })
+                .region(DEFAULT_REGION)
+                .credentialsProvider(
+                        StaticCredentialsProvider.create(
+                                AwsBasicCredentials.create(ACCESS_KEY_ID, ACCESS_KEY_SECRET)))
+                .endpointOverride(URI.create(LOCALSTACK_ENDPOINT))
                 .build();
     }
 
     @Bean
-    @Autowired
     public String notificationQueueUrl(SqsClient sqsClient) {
-        return sqsClient.getQueueUrl(builder -> builder.queueName("email-notifications")).queueUrl();
+        return sqsClient.getQueueUrl(builder -> builder.queueName("email-notification-queue")).queueUrl();
     }
-
 }
